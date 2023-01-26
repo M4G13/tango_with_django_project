d8376562324dc7ce8cc773f05ccb8f847b7de03a
Magnus Reid <2668261R@student.gla.ac.uk>
2023-01-26 15:47:40 +0000

    6.3 fix links

--- rango/views.py
+++ rango/views.py
@@ -5,8 +5,8 @@ from rango.models import Category
 from rango.models import Page
 
 def index(request):
-    category_list = Page.objects.order_by('-views')[:5]
-    page_list = Category.objects.order_by('-likes')[:5]
+    page_list = Page.objects.order_by('-views')[:5]
+    category_list = Category.objects.order_by('-likes')[:5]
     context_dict = {}
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
