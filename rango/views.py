d57079997f15f8df406517a2f87600a08e111814
Magnus Reid <2668261R@student.gla.ac.uk>
2023-02-03 18:21:22 +0000

    10.9 visitor cookie

--- rango/views.py
+++ rango/views.py
@@ -9,19 +9,27 @@ from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 
 from datetime import datetime
 
-def visitor_cookie_handler(request, response):
-    visits = int(request.COOKIES.get('visits', '1'))
-    last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+def visitor_cookie_handler(request):
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request,
+                                                'last_visit',
+                                                str(datetime.now()))
     last_visit_time = datetime.strptime(last_visit_cookie[:-7],
                                         '%Y-%m-%d %H:%M:%S')
 
     if (datetime.now() - last_visit_time).days > 0:
         visits = visits + 1
-        response.set_cookie('last_visit', str(datetime.now()))
+        request.session['last_visit'] = str(datetime.now())
     else:
-        response.set_cookie('last_visit', last_visit_cookie)
+        request.session['last_visit'] = last_visit_cookie
 
-    response.set_cookie('visits', visits)
+    request.session['visits'] = visits
 
 def index(request):
     page_list = Page.objects.order_by('-views')[:5]
@@ -31,16 +39,15 @@ def index(request):
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
-    context_dict['visits'] = int(request.COOKIES.get('visits', '1'))
 
-    response = render(request, 'rango/index.html', context=context_dict)
+    visitor_cookie_handler(request)
 
-    visitor_cookie_handler(request, response)
-
-    return response
+    return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    context_dict = {'boldmessage': 'This tutorial has been put together by Magnus'}
+    visitor_cookie_handler(request)
+    context_dict = {'boldmessage': 'This tutorial has been put together by Magnus',
+                    'visits': request.session['visits']}
     return render(request, 'rango/about.html', context=context_dict)
 
 def show_category(request, category_name_slug):
